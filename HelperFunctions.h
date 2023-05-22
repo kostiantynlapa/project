3c29aca4e4cd3344b8d7892dc800d1eef65ff513
jonathankenway <kostiantynlapa@gmail.com>
2023-05-22 10:39:11 +0300

    project

--- HelperFunctions.h
+++ HelperFunctions.h
@@ -9,8 +9,10 @@
 
 using namespace std;
 
+
 enum SystemMessages { ERROR, INFO, INPUT, START };
 using namespace std;
+
 
 void printSystemMessage(const string& sentence, SystemMessages msg, bool deadline = false); // function to print system msg with different background colors using ANSI code
 void getYesNoAnswer(const string& question, string& answer); // method to get yes/no answer from user
